8f7403ac9aa6758246212ff73d026e3495eb6862
Japa2911 <37105962+Japa2911@users.noreply.github.com>
2018-03-06 07:33:35 -0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,32 @@
+const Discord = require ('discord.js');
+const bot = new Discord.Client();
+
+bot.login('NDIwMzU3NDYyODIzMTQxMzk2.DX_7lw.VIl6a0GLgnL3hjidJeZynHlouJE');
+
+bot.on('message', message => {
+let respondeObject = {
+    "oi" : "Olá, como vai?",
+    "!site" : "www.minecraftmania.com.br",
+    "!forum" : "forum.minecraftmania.com.br",
+    "!comandos" : "Lista de Comandos:\n \n !site\n !forum\n !ip\n !Seg\n !Ter\n !Qua\n !Qui\n !Sex\n !Sab\n !Dom",
+    "!ip" : "jogar.minecraftmania.com.br",
+    "!seg" : "Evento de Segunda-Feira:\n \n 9:30 >> Corrida Maniaca\n 11:00 >> Corda Bamba\n 15:00 >> Labirinto Maniaco\n 17:30 >> Spleff\n 20:00 >> Aventura Maniaca",
+    "!ter" : "Evento de Terça-Feira:\n \n 9:30 >> Corrida Maniaca\n 11:00 >> Corda Bamba\n 15:00 >> Labirinto Maniaco\n 17:30 >> Spleff\n 20:00 >> Aventura Maniaca",
+    "!qua" : "Evento de Quarta-Feira:\n \n 9:30 >> Corrida Maniaca\n 11:00 >> Corda Bamba\n 15:00 >> Labirinto Maniaco\n 17:30 >> Spleff\n 20:00 >> Aventura Maniaca",
+    "!qui" : "Evento de Quinta-Feira:\n \n 9:30 >> Corrida Maniaca\n 11:00 >> Corda Bamba\n 15:00 >> Labirinto Maniaco\n 17:30 >> Spleff\n 20:00 >> Aventura Maniaca",
+    "!sex" : "Evento de Sexta,-Feira:\n \n 9:30 >> Corrida Maniaca\n 11:00 >> Corda Bamba\n 15:00 >> Labirinto Maniaco\n 17:30 >> Spleff\n 20:00 >> Aventura Maniaca",
+    "!sab" : "Evento de Sabado:\n \n 9:30 >> Corrida Maniaca\n 11:00 >> Corda Bamba\n 15:00 >> Guerra de Clans\n 17:30 >> Spleff\n 20:00 >> Aventura Maniaca",
+    "!dom" : "Evento de Domingo:\n \n 9:30 >> Corrida Maniaca\n 11:00 >> Corda Bamba\n 15:00 >> Evento VIP (somente vips)\n 17:30 >> Spleff\n 20:00 >> Aventura Maniaca"
+};
+
+if(respondeObject[message.content]){
+    message.channel.send(respondeObject[message.content]);
+}
+
+let role = message.guild.roles.find("name", "Config Geral");
+if(message.member.roles.has(role.id) && message.content.startsWith("!limpar")){
+    msgDel = 100;
+    let numberMessages = parseInt(msgDel);
+    message.channel.fetchMessages({limit: numberMessages}).then(messages => message.channel.bulkDelete(messages));
+}
+});
